fcb1d1d64ce382d93a7150dc7aaa99aa42634edb
phubierd <phubierd@gmail.com>
2021-05-21 16:29:19 +0700

    game2

---
+++ src/ReduxDemo/BTGameOanTuTi/BTGameOanTuTi.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import style from ''
+export default class BTGameOanTuTi extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/ReduxDemo/BTGameOanTuTi/KQTroChoi.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class KQTroChoi extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
